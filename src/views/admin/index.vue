fa701d7b123fd85c719c43fb6a6a113134b15062
lan <839528187@qq.com>
2018-10-15 16:19:04 +0800

    cq

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -6,7 +6,7 @@
       <el-button size="small" type="primary" style="margin-left: 10px; margin-bottom:1px;" plain @click="getList()">搜索</el-button>
     </div>
 
-    <div style="margin-bottom:1px"></div>
+    <div style="margin-bottom:1px"/>
     <!-- 表格数据 -->
     <el-table
       v-loading="listLoading"
@@ -16,77 +16,68 @@
       fit
       highlight-current-row
       style="width: 100%;">
-        <el-table-column label="id" prop="id" align="center" width="60px"></el-table-column>
-        <el-table-column label="用户名" prop="username" align="center"></el-table-column>
-        <el-table-column label="邮件地址" prop="email" align="center"></el-table-column>
-        <el-table-column label="创建时间" prop="createTime" align="center"></el-table-column>
-        <el-table-column label="状态" prop="status" align="center">
-          <template slot-scope="scope">
-            {{ scope.row.status == 1 ? '正常' : '待审核' }}
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" prop="status" align="center" class-name="small-padding fixed-width">
-          <template slot-scope="scope">
-            <el-row>
-              <el-button type="text" circle @click="handleUpdate(scope.row)">编辑</el-button>
-              <el-button type="text" disabled>删除</el-button>
-              <!-- <el-button type="primary" icon="el-icon-edit" circle @click="handleUpdate(scope.row)"></el-button>
+      <el-table-column label="id" prop="id" align="center" width="60px"/>
+      <el-table-column label="用户名" prop="username" align="center"/>
+      <el-table-column label="创建时间" prop="createdAt" align="center"/>
+      <el-table-column label="状态" prop="status" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.status == 1 ? '正常' : '待审核' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" prop="status" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-row>
+            <el-button type="text" circle @click="handleUpdate(scope.row)">编辑</el-button>
+            <el-button type="text" disabled>删除</el-button>
+            <!-- <el-button type="primary" icon="el-icon-edit" circle @click="handleUpdate(scope.row)"></el-button>
               <el-button type="danger" icon="el-icon-delete" circle></el-button> -->
           </el-row>
-          </template>  
-        </el-table-column>
-      </el-table>
-      <!-- 分页 -->
-      <!-- <div>
-        <el-pagination v-show="total>10" :current-page="listQuery.page" :page-size="listQuery.limit" :total="total" background layout="prev, pager, next" style="text-align: right;" @size-change="handleSizeChange" @current-change="handleCurrentChange"> 
-      </el-pagination>  
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <!-- <div>
+        <el-pagination v-show="total>10" :current-page="listQuery.page" :page-size="listQuery.limit" :total="total" background layout="prev, pager, next" style="text-align: right;" @size-change="handleSizeChange" @current-change="handleCurrentChange">
+      </el-pagination>
       </div> -->
 
-      <div style="margin-bottom:20px"></div>
-      <div class="block">
-          <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page.sync="listQuery.page"
-            :page-size="listQuery.limit"
-            layout="prev, pager, next, jumper"
-            prev-text="上一页"
-            next-text="下一页"
-            :total="total"
-            style="text-align: right;">
-          </el-pagination>
-        </div>
+    <div style="margin-bottom:20px"/>
+    <div class="block">
+      <el-pagination
+        :current-page.sync="listQuery.page"
+        :page-size="listQuery.limit"
+        :total="total"
+        layout="prev, pager, next, jumper"
+        prev-text="上一页"
+        next-text="下一页"
+        style="text-align: right;"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"/>
+    </div>
 
-
-      <!-- 弹出框 -->
-      <el-dialog :title="dialogStatus" :visible.sync="dialogFormVisible" style="width: 1110px; margin-left: auto; margin-right: auto;">
-        <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-          <el-form-item label="用户名" prop="username">
-            <el-input v-model="temp.username" value="username" :disabled="dusabked" placeholder="用户名"/>
-          </el-form-item>
-          <el-form-item label="邮件" prop="email">
-            <el-input v-model="temp.email" value="email" :disabled="dusabked" placeholder="邮件"/>
-          </el-form-item>
-          <el-form-item label="密码" prop="password">
-            <el-input type="password" v-model="temp.password" placeholder="密码"/>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取消</el-button>
-          <el-button type="primary" @click="dialogStatus==='添加账户'?createData():updateData()">提交</el-button>
-        </div>
-      </el-dialog>
+    <!-- 弹出框 -->
+    <el-dialog :title="dialogStatus" :visible.sync="dialogFormVisible" style="width: 1110px; margin-left: auto; margin-right: auto;">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="temp.username" :disabled="dusabked" value="username" placeholder="用户名"/>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="temp.password" type="password" placeholder="密码"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogStatus==='添加账户'?createData():updateData()">提交</el-button>
+      </div>
+    </el-dialog>
 
   </div>
 </template>
 
 <script>
-import { list, create, update } from "@/api/admin";
+import { list, create, update } from '@/api/admin'
 export default {
-  created() {
-    this.getList();
-  },
-  //数据
+  // 数据
   data() {
     return {
       tableKey: 0,
@@ -95,129 +86,130 @@ export default {
       listLoading: true,
       dialogFormVisible: false,
       dusabked: false,
-      dialogStatus: "",
+      dialogStatus: '',
       listQuery: {
         page: 1,
         limit: 10
       },
       temp: {
-        id: "",
-        username: "",
-        password: "",
-        email: "",
-        status: "",
-        createTime: "",
+        id: '',
+        username: '',
+        password: '',
+        status: '',
+        createdAt: '',
         keyword: ''
       },
       rules: {
         username: [
-          { required: true, message: "用户名不能为空", trigger: "change" }
+          { required: true, message: '用户名不能为空', trigger: 'change' }
         ],
         password: [
-          { required: true, message: "密码不能为空", trigger: "change" }
+          { required: true, message: '密码不能为空', trigger: 'change' }
         ],
-        email: [{ required: true, message: "邮箱", trigger: "change" }]
+        email: [{ required: true, message: '邮箱', trigger: 'change' }]
       }
-    };
+    }
+  },
+  created() {
+    this.getList()
   },
 
-  //方法
+  // 方法
   methods: {
-    //获取列表
+    // 获取列表
     getList() {
-      this.listLoading = true;
-      list(this.listQuery,this.temp.keyword).then(response => {
-        this.list = response.data.data;
-        this.total = response.data.pageSize;
+      this.listLoading = true
+      list(this.listQuery, this.temp.keyword).then(response => {
+        this.list = response.data.data
+        this.total = response.data.pageSize
 
         // Just to simulate the time of the request
         setTimeout(() => {
-          this.listLoading = false;
-        }, 1.5 * 200);
-      });
+          this.listLoading = false
+        }, 1.5 * 200)
+      })
     },
 
     handleFilter() {
-      this.listQuery.page = 1;
-      this.getList();
+      this.listQuery.page = 1
+      this.getList()
     },
     handleSizeChange(val) {
-      this.listQuery.limit = val;
-      this.getList();
+      this.listQuery.limit = val
+      this.getList()
     },
     handleCurrentChange(val) {
-      this.listQuery.page = val;
-      this.getList();
+      this.listQuery.page = val
+      this.getList()
     },
 
     resetTemp() {
       this.temp = {
-        id: "",
-        username: "",
-        password: "",
-        email: ""
-      };
+        id: '',
+        username: '',
+        password: ''
+      }
     },
 
-    //添加用户
+    // 添加用户
     handleCreate() {
-      this.resetTemp();
-      this.dialogStatus = "添加账户";
-      this.dialogFormVisible = true;
+      this.resetTemp()
+      this.dialogStatus = '添加账户'
+      this.dialogFormVisible = true
       this.$nextTick(() => {
-        this.$refs["dataForm"].clearValidate();
-      });
+        this.$refs['dataForm'].clearValidate()
+      })
     },
     createData() {
-      this.$refs["dataForm"].validate(valid => {
+      this.$refs['dataForm'].validate(valid => {
         if (valid) {
           create(this.temp).then(data => {
-            this.list.unshift(this.temp);
-            this.dialogFormVisible = false;
-            if (data.code == 200) {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            if (data.code === 200) {
               this.$notify({
                 message: data.msg,
-                type: "success"
-              });
-              this.getList();
+                type: 'success'
+              })
+              this.getList()
             } else {
-              return false;
+              return false
             }
-          });
+          })
         }
-      });
+      })
     },
 
-    //编辑
+    // 编辑
     handleUpdate(row) {
-      this.temp = Object.assign({}, row); // copy obj
-      this.dialogStatus = "编辑账户";
-      this.dialogFormVisible = true;
-      this.dusabked = true;
+      this.temp = Object.assign({}, row) // copy obj
+      this.dialogStatus = '编辑账户'
+      this.dialogFormVisible = true
+      this.dusabked = true
       this.$nextTick(() => {
-        this.$refs["dataForm"].clearValidate();
-      });
+        this.$refs['dataForm'].clearValidate()
+      })
     },
     updateData() {
-      this.$refs["dataForm"].validate(valid => {
+      this.$refs['dataForm'].validate(valid => {
         if (valid) {
           update(this.temp, this.temp.id).then(data => {
-            this.list.unshift(this.temp);
-            this.dialogFormVisible = false;
-            if (data.code == 200) {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            if (data.code === 200) {
               this.$notify({
                 message: data.msg,
-                type: "success"
-              });
-              this.getList();
+                type: 'success'
+              })
+              this.getList()
             } else {
-              return false;
+              return false
             }
-          });
+          })
         }
-      });
+      })
     }
   }
-};
+}
 </script>
 
